46a0b4bb2f91339a805db62fa9b33a8e80dd1148
Ozzy Simpson <14060578+ozzy-simpson@users.noreply.github.com>
2022-05-19 13:02:47 -0700

    Update display_metro_graphics2.py

--- display_metro_graphics2.py
+++ display_metro_graphics2.py
@@ -12,7 +12,6 @@ BLACK = (0, 0, 0)
 
 show = 4
 
-
 class Metro_Graphics:
     def __init__(self, display):
 
@@ -21,26 +20,22 @@ class Metro_Graphics:
         self.large_font = large_font
 
         self.display = display
-
-        self._metro_icon = None
-        self._destination_name = None
-        self._location_name = None
-        self._arrival_minutes = None
-        self._line1 = None
-        self._progress = None
-        self._has_arrived = None
+        
+        for i in range(show):
+            self._line[i] = None
+            self._dest[i] = None
+            self._min[i] = None
 
     def display_metro(self, metro_status):
-    
-        self._line1 = metro_status['Trains'][0]['Line']
-        self._line2 = metro_status['Trains'][1]['Line']
-        self._line3 = metro_status['Trains'][2]['Line']
-    
-        self._dest1 = metro_status['Trains'][0]['Destination']
-        self._dest2 = metro_status['Trains'][1]['Destination']
-        self._dest3 = metro_status['Trains'][2]['Destination']   
         
-        self._min1 = metro_status['Trains'][0]['Min']
+        # Shorten show if there are fewer trains right now!
+        if len(metro_status['Trains']) < show:
+            show = len(metro_status['Trains'])
+            
+        for i in range(show):
+            self._line[i] = metro_status['Trains'][i]['Line']
+            self._dest[i] = metro_status['Trains'][i]['Destination']   
+            self._min[i] = metro_status['Trains'][i]['Min']
     
         self.update_time()
         self.update_display()
@@ -91,26 +86,24 @@ class Metro_Graphics:
             # Line
             draw.text(
                 (0, yVal),
-                self._line1,
+                self._line[i],
                 font=self.medium_font,
                 fill=BLACK,
             )
-            (font_width, font_height) = medium_font.getsize(self._line1)
         
             # Destination
             draw.text(
                 (destX, yVal),
-                self._dest1,
+                self._dest[i],
                 font=self.medium_font,
                 fill=BLACK,
             )
-            (font_width, font_height) = medium_font.getsize(self._dest1)
-        
+            
             # Minutes
             (font_width, font_height) = medium_font.getsize(self._min1)
             draw.text(
                 (self.display.height - font_width - 5, yVal),
-                self._min1,
+                self._min[i],
                 font=self.medium_font,
                 fill=BLACK,
             )
